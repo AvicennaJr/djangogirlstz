a3c6942637d8094b343de8f5186ec1b16b3dcb84
Avicenna <87450618+AvicennaJr@users.noreply.github.com>
2024-11-12 17:54:57 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7aqm41@)gg^m91gmagn*el^-ubmhg!#h@iypl8m04%je37gs-3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
